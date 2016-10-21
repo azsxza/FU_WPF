f0115ca69b4e9d81e813705a501b35f1a7914b77
chengk <aaz63113270@outlook.com>
2016-10-21 17:12:33 +0800

    333

--- FU_UWP/MainWindow.xaml.cs
+++ FU_UWP/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -29,6 +30,50 @@ namespace FU_UWP
         {
             if (e.Key == Key.Q)
                 this.Close();
+        }
+        void MainPageUpdate1()
+        {
+            ThicknessAnimation anim = new ThicknessAnimation();
+            anim.From = new Thickness(462, 255, 1058, 0);
+            anim.To = new Thickness(10, 465, 1710, 0);
+            anim.Duration = TimeSpan.FromSeconds(0.36);
+            paizhao.BeginAnimation(MarginProperty, anim);
+
+            ThicknessAnimation anim2 = new ThicknessAnimation();
+            anim2.From = new Thickness(1127, 250, 393, 430);
+            anim2.To = new Thickness(1710, 460, 10, 420);
+            anim2.Duration = TimeSpan.FromSeconds(0.36);
+            shangchuan.BeginAnimation(MarginProperty, anim2);
+
+            DoubleAnimation daV = new DoubleAnimation(400, 200, new Duration(TimeSpan.FromSeconds(0.36)));
+            paizhao.BeginAnimation(WidthProperty, daV);
+            DoubleAnimation daV2 = new DoubleAnimation(400, 200, new Duration(TimeSpan.FromSeconds(0.36)));
+            paizhao.BeginAnimation(HeightProperty, daV2);
+
+            DoubleAnimation daV3 = new DoubleAnimation(400, 200, new Duration(TimeSpan.FromSeconds(0.36)));
+            shangchuan.BeginAnimation(WidthProperty, daV3);
+            DoubleAnimation daV4 = new DoubleAnimation(400, 200, new Duration(TimeSpan.FromSeconds(0.36)));
+            shangchuan.BeginAnimation(HeightProperty, daV4);
+        }
+
+        private void ImageButtonMouseEnter(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void ImageButtonMouseLeave(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void paizhao_Click(object sender, RoutedEventArgs e)
+        {
+            MainPageUpdate1();
+            ((ImageBrush)paizhao.Background).ImageSource = new BitmapImage(new Uri(@"C:\Users\chengk\Desktop\FU_WPF\FU_UWP\images\图标\返回.png"));
+        }
+
+        private void shangchuan_Click(object sender, RoutedEventArgs e)
+        {
+            MainPageUpdate1();
         }
     }
 }
